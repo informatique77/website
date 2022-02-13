d2dde5d363b8dc2980b59cca791628b661f5fbd5
GIBELLI Christophe <cgibelli@tequilarapido.com>
2022-02-13 22:00:08 +0100

    Rework

--- src/pages/a-propos.js
+++ src/pages/a-propos.js
@@ -34,7 +34,23 @@ const OurJob = ({ data }) => {
             des <span style={{ color: "#e74c3c" }}>examens d'imagerie médicale</span> ou de
             <span style={{ color: "#e74c3c" }}> biologie médicale</span> pouvant être réalisé sur place.
           </p>
-        </div>
+          <div className="pink-container">
+            <p>
+              Nous pouvons vous orienter vers notre réseau de spécialistes pour compléter la prise en charge pour des avis
+              complémentaires.
+            </p>
+          </div>
+      </div>
+      <div className="our-job-img-container">      
+          <StaticImage
+            src="../assets/images/ourJobs/notre-equipe.jpg"
+            width={600}
+            quality={100}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Camera téléconsultations"
+            placeholder="blurred"
+          />   
+      </div>
       <div className="name-container">
         <div className="section-one">
           <ul>
@@ -59,114 +75,6 @@ const OurJob = ({ data }) => {
               )
             })}
           </ul>
-        </div>
-      </div>
-      <div className="our-job-section">
-        <div className="our-job-img">      
-            <StaticImage
-              src="../assets/images/ourJobs/notre-metier.jpg"
-              width={400}
-              quality={100}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="Camera téléconsultations"
-              placeholder="blurred"
-            />   
-        </div>
-        <div className="our-job-resume">
-          <div className="part-one">
-            <div className="our-job-text">
-              <p>
-                {" "}
-                Nous effectuons des consultations sans rendez-vous pour vous
-                prendre en charge.
-                <br></br>
-                <br></br>
-                Si besoin, nous pouvons vous proposer d'effectuer des examens
-                d'imagerie médicale ou de biologie médicale pouvant être réalisé
-                sur place.
-              </p>
-            </div>
-          </div>
-          <div className="part-two">
-            <div className="our-job-text">
-              <p>
-                <span>- Médecine générale</span>
-                <br></br>
-                <span>- Médecine d'urgence</span>
-                <br></br>
-                <span>- Gérontologie</span>
-                <br></br>
-                <span>- Pédiatrie</span>
-                <br></br>
-                <span>- Suture de plaie</span>
-                <br></br>
-                <span>- Entorse et fracture</span>
-                <br></br>
-              </p>
-            </div>
-          </div>
-          <div className="part-three">
-            <div className="our-job-text">
-              <p>
-                Nous pouvons vous orienter vers notre réseau de spécialistes
-                pour complèter la prise en charge pour des avis complémentaires.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="prices-section">
-        <span className="small-title">Tarifs</span>
-        <div>
-          <StaticImage
-            src="../assets/images/icons/euro-100.png"
-            width={100}
-            quality={100}
-            formats={["AUTO", "WEBP", "AVIF"]}
-            alt="Camera téléconsultations"
-            placeholder="blurred"
-          />
-        </div>
-        <div className="pricesTable-container">
-          <PriceTable
-            country="France"
-            image={
-              <StaticImage
-                src="../assets/images/flag/france.png"
-                width={96}
-                quality={100}
-                formats={["AUTO", "WEBP", "AVIF"]}
-                alt="Drapeau de Monaco"
-                placeholder="blurred"
-              />
-            }
-            resume={
-              <ul>
-                <li>Tarifs conventionnés de la sécurité sociale</li>
-                <li>Secteur 1</li>
-                <li>Sans dépassement d'honoraire</li>
-              </ul>
-            }
-          />
-          <PriceTable
-            country="Monaco"
-            image={
-              <StaticImage
-                src="../assets/images/flag/monaco.png"
-                width={96}
-                quality={100}
-                formats={["AUTO", "WEBP", "AVIF"]}
-                alt="Drapeau de la France"
-                placeholder="blurred"
-              />
-            }
-            resume={
-              <ul>
-                <li>Conventionné</li>
-                <li>Caisse de Monaco pour les assurés</li>
-              </ul>
-            }
-          />
         </div>
       </div>
     </Layout>
