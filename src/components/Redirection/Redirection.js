c838c73b4f39bacfcd2f6991b282149481b2514e
GIBELLI Christophe <cgibelli@tequilarapido.com>
2023-01-29 16:26:41 +0100

    Ajout du "R" majuscule sur redirection

--- src/components/Redirection/Redirection.js
+++ src/components/Redirection/Redirection.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 const Redirection = () => {
     return (
-        <div className="redirection">
+        <div className="Redirection">
             <h2>Consultations 7 sur 7 devient</h2>
             <a href="https://www.c7sante.org/">C<span class="red-seven">7</span></a>
         </div>
--- src/pages/nos-tarifs.js
+++ src/pages/nos-tarifs.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "../components/Layout/Layout"
 
-import Redirection from "../components/redirection/Redirection"
+import Redirection from "../components/Redirection/Redirection"
 
 
 const Prices = () => {
