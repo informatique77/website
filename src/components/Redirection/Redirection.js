7b226577fe1982063da1df41f12b50626a1a8200
GIBELLI Christophe <cgibelli@tequilarapido.com>
2023-01-30 21:58:04 +0100

    Ajout logo sur bouton de redirection

--- src/components/Redirection/Redirection.js
+++ src/components/Redirection/Redirection.js
@@ -1,10 +1,13 @@
 import * as React from "react"
+import logo from "../../assets/images/small-logo.png"
 
 const Redirection = () => {
     return (
         <div className="redirection">
             <h2>Consultations 7 sur 7 devient</h2>
-            <a href="https://www.c7sante.org/">C<span class="red-seven">7</span></a>
+            <a href="https://www.c7sante.org/">
+                <img src={logo} />
+            </a>
         </div>
     )
 }
